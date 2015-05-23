66db5d1e488c40424db9eb1384784337747c257d
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-05-23 20:22:08 +0200

    Typo fix

--- tests/app/config/database.php
+++ tests/app/config/database.php
@@ -6,7 +6,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'gzero-test'),
+            'database'  => env('DB_DATABASE', 'gzero-tests'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
