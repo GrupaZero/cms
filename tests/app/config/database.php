26424ff8196982dafe4b8a1207d56d175cdb765f
mateuszurbanowicz <urbanowiczmateusz89@gmail.com>
2015-11-24 16:04:16 +0100

    tests

--- tests/app/config/database.php
+++ tests/app/config/database.php
@@ -8,7 +8,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'gzero-tests'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
