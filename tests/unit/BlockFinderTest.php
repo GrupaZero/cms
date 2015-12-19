beb851b33070baa9b154e9c8f48d8af747bef5f5
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2015-12-19 13:13:34 +0100

    BlockFinder Unit tests

--- tests/unit/BlockFinderTest.php
+++ tests/unit/BlockFinderTest.php
@@ -44,17 +44,83 @@ class BlockFinderTest extends \TestCase {
      */
     public function it_finds_correct_block()
     {
-        $block1         = new Block();
-        $block1->id     = 1;
+        // Our content path
+        $contentPath = '1/2/3/4/5/6/';
+        // Block visible on all pages (get by SQL query)
+        $block1     = new Block();
+        $block1->id = 1;
+        // Block visible on all root children's pages
         $block2         = new Block();
         $block2->id     = 2;
         $block2->filter = ['+' => ['1/*']];
+        // Block hidden on all root children's pages
+        $block3         = new Block();
+        $block3->id     = 3;
+        $block3->filter = ['-' => ['1/*']];
+        // Block visible only on that content
+        $block4         = new Block();
+        $block4->id     = 4;
+        $block4->filter = ['+' => ['1/2/3/4/5/6/']];
+        // Block hidden only on that content
+        $block5         = new Block();
+        $block5->id     = 5;
+        $block5->filter = ['-' => ['1/2/3/4/5/6/']];
+        // Block visible for all content parents children's
+        $block6         = new Block();
+        $block6->id     = 6;
+        $block6->filter = ['+' => ['1/2/3/*']];
+        // Block hidden for all content parents children's
+        $block7         = new Block();
+        $block7->id     = 7;
+        $block7->filter = ['-' => ['1/2/3/*']];
+
+        // Check for repository method call
         $this->repo->shouldReceive('getBlocks')->andReturn(
             [
                 $block1,
                 $block2,
+                $block3,
+                $block4,
+                $block5,
+                $block6,
+                $block7,
             ]
         );
-        $this->assertContains(2, $this->finder->getBlocksIds('1/2/'));
+        // Should not contain block visible on all pages those blocks are get by SQL query
+        $this->assertNotContains(1, $this->finder->getBlocksIds($contentPath));
+        //  Block should be visible on all root children's pages
+        $this->assertContains(2, $this->finder->getBlocksIds($contentPath));
+        //  Block should be hidden on all root children's pages
+        $this->assertNotContains(3, $this->finder->getBlocksIds($contentPath));
+        //  Block should be visible only on that content
+        $this->assertContains(4, $this->finder->getBlocksIds($contentPath));
+        //  Block should be hidden only on that content
+        $this->assertNotContains(5, $this->finder->getBlocksIds($contentPath));
+        //  Block should be visible for all content parents children's
+        $this->assertContains(6, $this->finder->getBlocksIds($contentPath));
+        //  Block should be hidden for all content parents children's
+        $this->assertNotContains(7, $this->finder->getBlocksIds($contentPath));
+    }
+
+
+    /**
+     * @test
+     */
+    public function it_uses_correct_order_of_operations()
+    {
+        // Our content path
+        $contentPath    = '1/2/';
+        $block1         = new Block();
+        $block1->id     = 1;
+        $block1->filter = ['+' => ['1/*'], '-' => [$contentPath]];
+
+        // Check for repository method call
+        $this->repo->shouldReceive('getBlocks')->andReturn(
+            [
+                $block1
+            ]
+        );
+        // Block should be hidden because of order operation
+        $this->assertNotContains(1, $this->finder->getBlocksIds($contentPath));
     }
 }
