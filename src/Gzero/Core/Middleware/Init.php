581c3a1dc266d69dd00a5695d386069f44f4a875
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2016-07-17 20:28:46 +0200

    Added Init middleware

---
+++ src/Gzero/Core/Middleware/Init.php
@@ -0,0 +1,44 @@
+<?php namespace Gzero\Core\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class Init
+ *
+ * @package    Gzero\Core
+ * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
+ * @copyright  Copyright (c) 2015, Adrian Skierniewski
+ */
+class Init {
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request Request object
+     * @param \Closure                 $next    Next middleware
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (str_contains($request->getRequestUri(), 'index.php')) {
+            return new RedirectResponse(url(preg_replace('#index.php(/)?#', '', $request->fullUrl())));
+        }
+        return $next($request);
+    }
+
+}
