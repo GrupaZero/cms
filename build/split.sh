fbb0ebb7da3f39b670a933d239f88628f92ec429
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2016-11-22 17:35:07 +0100

    composer update plus faster git split

--- build/split.sh
+++ build/split.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git subsplit init git@github.com:GrupaZero/cms.git
-git subsplit publish src/Gzero/Core:git@github.com:GrupaZero/core.git
-git subsplit publish src/Gzero/Entity:git@github.com:GrupaZero/entity.git
-git subsplit publish src/Gzero/Repository:git@github.com:GrupaZero/repository.git
-git subsplit publish src/Gzero/Validator:git@github.com:GrupaZero/validator.git
+git subsplit publish src/Gzero/Core:git@github.com:GrupaZero/core.git &
+git subsplit publish src/Gzero/Entity:git@github.com:GrupaZero/entity.git &
+git subsplit publish src/Gzero/Repository:git@github.com:GrupaZero/repository.git &
+git subsplit publish src/Gzero/Validator:git@github.com:GrupaZero/validator.git &
+wait
 rm -rf .subsplit/
