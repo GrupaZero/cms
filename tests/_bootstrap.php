519712089db73b89a7765f4345fef1aedcc61b2f
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2017-02-21 09:28:13 +0100

    Added trusted proxy to init middleware

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 
+if (file_exists(dirname(__DIR__) . '/.env.testing')) {
+    (new \Dotenv\Dotenv(dirname(__DIR__), '.env.testing'))->load();
+}
+
 \Codeception\Configuration::$defaultSuiteSettings['modules']['config']['Db'] = [
     'dsn'      => 'pgsql:host=' . env('DB_HOST', 'localhost') .
         ';port=' . env('DB_PORT', 5432) .
