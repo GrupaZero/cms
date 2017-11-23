939f2b3151757fce70ddf125a3c6816072b62740
Aleksandra Augustyn <a.augustyn@grupazero.pl>
2017-11-23 17:45:58 +0100

    get Ancestors test

--- tests/unit/repositories/ContentReadRepositoryTest.php
+++ tests/unit/repositories/ContentReadRepositoryTest.php
@@ -2,7 +2,7 @@
 
 use Codeception\Test\Unit;
 use Gzero\Cms\Repositories\ContentReadRepository;
-use Gzero\Core\Models\Route;
+use Gzero\Core\Models\Language;
 use Gzero\Core\Models\RouteTranslation;
 
 class ContentReadRepositoryTest extends Unit {
@@ -40,7 +40,56 @@ class ContentReadRepositoryTest extends Unit {
         $this->assertEquals('en', $routeTranslation->language_code);
         // Only active route translations
         $this->assertNull($this->repository->getByPath('example-title', 'en', true));
+    }
 
+    /** @test */
+    public function getAncestorsTitlesAndPaths()
+    {
+        $language    = Language::find(['code' => 'en'])->first();
+        $grandParent = $this->tester->haveContent([
+            'type'         => 'category',
+            'translations' => [
+                [
+                    'language_code' => 'en',
+                    'title'         => 'Grand parent title'
+                ]
+            ]
+        ]);
+        $parent      = $this->tester->haveContent([
+            'type'         => 'category',
+            'translations' => [
+                [
+                    'language_code' => 'en',
+                    'title'         => 'Parent title'
+                ]
+            ]
+        ]);
+        $child       = $this->tester->haveContent([
+            'type'         => 'content',
+            'translations' => [
+                [
+                    'language_code' => 'en',
+                    'title'         => 'Child title'
+                ]
+            ]
+        ]);
+
+        $parent->setChildOf($grandParent);
+        $child->setChildOf($parent);
+
+        $titles = $this->repository->getAncestorsTitlesAndPaths($child, $language)->toArray();
+
+        $this->assertEquals('Grand parent title', $titles[0]->title);
+        $this->assertEquals('grand-parent-title', $titles[0]->path);
+        $this->assertEquals("$grandParent->id/", $grandParent->path);
+
+        $this->assertEquals('Parent title', $titles[1]->title);
+        $this->assertEquals('parent-title', $titles[1]->path);
+        $this->assertEquals("$grandParent->id/$parent->id/", $parent->path);
+
+        $this->assertEquals('Child title', $titles[2]->title);
+        $this->assertEquals('child-title', $titles[2]->path);
+        $this->assertEquals("$grandParent->id/$parent->id/$child->id/", $child->path);
     }
 }
 
