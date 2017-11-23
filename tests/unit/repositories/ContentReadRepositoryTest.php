e406a810f2ceab4be4b3ebea05889befa3ed6a95
Aleksandra Augustyn <a.augustyn@grupazero.pl>
2017-11-23 18:34:30 +0100

    Added tests for getMany method

--- tests/unit/repositories/ContentReadRepositoryTest.php
+++ tests/unit/repositories/ContentReadRepositoryTest.php
@@ -3,7 +3,8 @@
 use Codeception\Test\Unit;
 use Gzero\Cms\Repositories\ContentReadRepository;
 use Gzero\Core\Models\Language;
-use Gzero\Core\Models\RouteTranslation;
+use Gzero\Core\Query\QueryBuilder;
+use Gzero\Core\Repositories\RepositoryValidationException;
 
 class ContentReadRepositoryTest extends Unit {
 
@@ -90,6 +91,137 @@ class ContentReadRepositoryTest extends Unit {
         $this->assertEquals('Child title', $titles[2]->title);
         $this->assertEquals('child-title', $titles[2]->path);
         $this->assertEquals("$grandParent->id/$parent->id/$child->id/", $child->path);
+    }
+
+    /** @test */
+    public function canAddConditionsToGetMany()
+    {
+        $this->tester->haveContents([
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'Example title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'Other title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'Inactive title',
+                        'is_active'     => false
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'pl',
+                        'title'         => 'Example polish title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'pl',
+                        'title'         => 'Other polish title'
+                    ]
+                ]
+            ]
+        ]);
+
+        $result = $this->repository->getMany(
+            (new QueryBuilder)
+                ->where('translations.is_active', '=', true)
+                ->where('translations.language_code', '=', 'en')
+                ->orderBy('id', 'asc')
+        );
+
+        $this->assertEquals(2, $result->count());
+        $this->assertEquals('en', $result->first()->translations->first()->language_code);
+        $this->assertEquals('en', $result->last()->translations->first()->language_code);
+    }
+
+    /** @test */
+    public function shouldCheckDependantField()
+    {
+        try {
+            $this->repository->getMany(
+                (new QueryBuilder)
+                    ->where('translations.is_active', '=', true)
+                    ->orderBy('id', 'asc')
+            );
+        } catch (RepositoryValidationException $exception) {
+            $this->assertEquals('Language code is required', $exception->getMessage());
+            return;
+        }
+
+        $this->fail('Exception should be thrown');
+    }
+
+    /** @test */
+    public function canPaginateResults()
+    {
+
+        $this->tester->haveContents([
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'A title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'B title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'C title'
+                    ]
+                ]
+            ],
+            [
+                'translations' => [
+                    [
+                        'language_code' => 'en',
+                        'title'         => 'D title'
+                    ]
+                ]
+            ]
+        ]);
+
+        $result = $this->repository->getMany(
+            (new QueryBuilder)
+                ->where('translations.is_active', '=', true)
+                ->where('translations.language_code', '=', 'en')
+                ->orderBy('translations.title', 'desc')
+                ->setPageSize(2)
+                ->setPage(2)
+        );
+
+        $this->assertEquals(2, $result->count());
+        $this->assertEquals(2, $result->perPage());
+        $this->assertEquals(2, $result->currentPage());
+        $this->assertEquals('B title', $result->first()->translations->first()->title);
+        $this->assertEquals('A title', $result->last()->translations->first()->title);
     }
 }
 
