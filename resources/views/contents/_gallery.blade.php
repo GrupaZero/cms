0b13fe3bb75b26eb1defcea1fd79fae5c9633af5
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2018-05-10 16:36:56 +0200

    Fixed colorbox in gallery view

--- resources/views/contents/_gallery.blade.php
+++ resources/views/contents/_gallery.blade.php
@@ -2,11 +2,12 @@
     <div class="row image-gallery">
         @foreach($images as $image)
             <div class="col-sm-6 col-md-3 image text-center mb-4">
-                <a href="{{croppaUrl($image->uploadPath())}}" class="thumbnail"
+                <a href="{{croppaUrl($image->uploadPath(), config('gzero.image.max_width'), config('gzero.image.max_height'),
+                ['resize'])}}" class="thumbnail colorbox"
                    rel="gallery" title="{{$image->title()}}">
-                    <img class="img-fluid colorbox"
+                    <img class="img-fluid"
                          title="{{$image->title() . ' ' . $image->description()}}"
-                         src="{{croppaUrl($image->uploadPath(), 280, 180)}}"
+                         src="{{croppaUrl($image->uploadPath(), 280, 180, ['resize'])}}"
                          alt="{{$image->title()}}">
                 </a>
             </div>
